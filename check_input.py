9a2e069ed1449b4a92113737feb5b8fba4b63969
willpitchers <willpitchers@gmail.com>
2019-07-11 11:37:47 +1000

    added check_input.py to catch duplicate input

---
+++ check_input.py
@@ -0,0 +1,20 @@
+#! /Users/williamp/anaconda3/bin/python
+
+import pandas as pd
+import numpy as np
+import glob, os, sys, pathlib
+
+# this script simply checks for duplicate isolates in the <input.tab> file
+
+samples = [ line.strip() for line in open( pathlib.Path( "sample_names.list" )).readlines() ] 
+
+sample_dupes = [x for n, x in enumerate( samples ) if x in samples[:n]]
+
+if len( sample_dupes ) > 0:
+    # print( "duplicate samples" )
+    sys.exit( 1 )
+else:
+    sys.exit( 0 )
+
+
+# other input-hygiene may appear here in future...
